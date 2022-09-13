f9ee4cb5ba8e73dc81db15f07eed13fe85c94ae2
RicardoFerraz <sourvivens@gmail.com>
2022-09-12 21:10:50 -0300

    Projeto criado

---
+++ sourlog-api/src/main/java/com/sourprojects/sourlog/SourlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.sourprojects.sourlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourlogApiApplication.class, args);
+	}
+
+}
---
+++ sourlog-api/src/test/java/com/sourprojects/sourlog/SourlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourprojects.sourlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
